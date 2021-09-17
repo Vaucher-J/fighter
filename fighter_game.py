a3c042dfa8bf71ece6dbb71c37caecbda9881a22
Julien VAUCHER <j.vaucher@workstation6.nsi.lan>
2021-09-17 09:53:17 +0200

    finalistation de la classe Fighter

--- fighter_game.py
+++ fighter_game.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from ramdom import randrange, uniform
+from random import randrange, uniform
 
 class Fighter: 
     """The base class of a Fighter"""
@@ -38,16 +38,35 @@ class Fighter:
     def __repr__(self):
         return self.get_name()
 
-    def punch(self, Fighter):
+    def punch(self, afighter):
         """
         punch Fighter
         return the  health points of Fighter
         """
-        points=int(uniform(0.7,1.0)*10*self_strenght()/Fighter.get_agility())
-        Fighter.__healthpoints = Fighter.get_healthpoints()-points
-        return Fighter.get_healthpoints()
+        points=int(uniform(0.7,1.0)*10*self.get_strenght()/afighter.get_agility())
+        afighter.__healthPoints = afighter.get_healthPoints()-points
+        return afighter.get_healthPoints()
 
 class Weapon:
     """The base class of a Weapon"""
 
-    def
+    def summary(self):
+        """Return the summary of a weapon"""
+
+        name='name:%s'%self.getName()
+        damage='dégat:%s'%self.getDamage()
+        ammos='munitions:%s'%self.getAmmos()
+        return '\n'.join([name, damage, ammos])
+
+
+    def shoot(self, aFighter):
+        """
+        Shoot a fighter and return the fighter health points
+        """
+        if self.getAmmos()>0:
+            lostPoints=int(self.getDamage()/aFighter.getAgility())
+            lostPoints=int(lostPoints*uniform(0.5,1)) # some random added
+            aFighter._healthPoints=aFighter.getHealthPoints()-lostPoints
+            self._ammos-=1#remove one ammo
+        return aFighter.getHealthPoints()
+
